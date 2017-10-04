464a545f15fca6e09508a49147162b68370ef2ec
Melanie Stoeckle <mpstoeckle@gmail.com>
2017-10-04 12:07:43 -0400

    got urls to show in console for cats array

--- app/main.js
+++ app/main.js
@@ -3,11 +3,37 @@ var demo = new Vue({
 	el: '#main',
 
 	data: {
-		layout: 'grid'
+		layout: 'grid',
+
+		// variable to store the URL results from the API in an array
+		cats: []
 	},
 
-	method: {
+	// Replaced `methods` with `mounted` so the function runs as soon as the page is loaded,
+	// instead of it running when the function is called
+	mounted: function() {
 
+		// console.log(this.cats); <-- can still access the global var 'cats'
+		var self = this;
+
+		axios.get("http://thecatapi.com/api/images/get?format=xml&results_per_page=6")
+			// `.then` is a promise to return these things in order
+			.then(function (response) {
+				// console.log(response.data); // shows raw response data
+
+				// copying from https://www.w3schools.com/xml/xml_parser.asp
+				var parser = new DOMParser
+				var data = parser.parseFromString(response.data, "text/xml");
+				// console.log(data); // shows data after it was parsed so we can understand it better
+
+				Array.from(data.getElementsByTagName('url'))
+				.forEach(function(url) {
+					// console.log(self.cats); // <-- cannot access the global var 'cats', in too deep, so created `var self = this` to pass it down deeper
+					self.cats.push(url.innerHTML);
+				});
+
+			});
+			console.log(self.cats);
 	}
 
 });
